0e067fda0b28a37514f9b36b5d6dc59c4761b359
nrosenst <nrosenst@users.noreply.github.com>
2013-11-04 16:11:21 -0500

    Update BasicGraph.java
    Add helper methods for basic testing of Analysis.java

--- src/src/BasicGraph.java
+++ src/src/BasicGraph.java
@@ -16,6 +16,18 @@ public class BasicGraph {
 		}
 	}
 	
+	public void addNode(int id) {
+	Node n = new Node(id);
+	allNodes.add(n);
+	}
+	
+	public void addEdge(Node start, Node dest) {
+		if (!start.connections.contains(dest)) {
+			start.connections.add(dest);
+			dest.connections.add(start);
+		}
+	}
+	
 	public List<Node> allNodes;
 	public BasicGraph() {
 		allNodes = new LinkedList<Node>();
