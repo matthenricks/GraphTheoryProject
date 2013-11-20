10122e88fd25d3f38e5a00114d09c0f1a566294c
nrosenst <nrosenst@users.noreply.github.com>
2013-11-19 19:43:00 -0500

    Update Runner.java

--- src/src/Runner.java
+++ src/src/Runner.java
@@ -1,34 +1,18 @@
 package src;
 
-import src.BasicGraph.Node;
-
 public class Runner {
-
-	static int counter = 0;
 	
 	public static void main(String[] args) {
-		BasicGraph graph = new BasicGraph();
 		
-		/*
-		 * sample test code, replace with graph generator when 
-		 * ready
-		 */
-		
-		Node[] n = new Node[9];
-		for (int i = 0; i < n.length; i++) {
-			graph.addNode(i);
-		}
-		for (int j = 0; j < n.length; j++) {
-			graph.addEdge(graph.allNodes.get(j), 
-					graph.allNodes.get(Math.abs((j + 1) % 9)));
-			
-			graph.addEdge(graph.allNodes.get(j), 
-					graph.allNodes.get(Math.abs((j + 2) % 9)));
-			
-		}
+		double density = 0.5;
+		BasicGraph graph = GraphCreator.createRandomConnectedGraph(25, 3, density);
+		System.out.println(graph);
 		
 		Analysis.analyzeStrictDecreasing(graph);
 		Analysis.analyzeDecreasingNeighbors(graph);
+		Analysis.analyzeIncreasingNeighbors(graph);
+		Analysis.analyzeStrictIncreasing(graph);
+		Analysis.analyzeNumColored(graph);
+		Analysis.analyzeHighColor(graph);
 	}
-
 }
