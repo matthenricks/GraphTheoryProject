9ec1d9b5bff949da3ef5c379ac7f5febeac45fdb
matthenricks <matthenricks@gmail.com>
2013-11-23 14:52:58 -0500

    Abstracted the functions into a common class to reduce code duplication and begin steps towards making an automated runner for each test! --Mostly needed for further use of code since it helps encapsolate current codebase

--- src/src/Analysis.java
+++ src/src/Analysis.java
@@ -1,9 +1,66 @@
 package src;
 
 import java.util.*;
+
 import src.BasicGraph.Node;
 
 public class Analysis {
+	
+	/*
+	 * Object that contains fields for needed metrics that will be collected by each function
+	 */
+	public static class StatTracker {
+		protected long my_duration;
+		protected int my_color_count;
+		protected boolean my_correctness;
+		
+		public StatTracker(long duration, int colors_used, boolean is_correct) {
+			my_duration = duration;
+			my_color_count = colors_used;
+			my_correctness = is_correct;
+		}
+		
+		public String toString() {
+			StringBuffer ret_str = new StringBuffer();
+			ret_str.append("Total Number of Colors Used: " + my_color_count + "\n");
+			ret_str.append("Total Time For Analysis (in nanoseconds): " + String.valueOf(my_duration) + "\n");
+			ret_str.append("Total Time For Analysis (in seconds): " + String.valueOf(my_duration/1000000000.0) + "\n");
+			ret_str.append("Successfully k-colored the graph: " + String.valueOf(my_correctness) + "\n");
+			return ret_str.toString();
+		}
+	};
+	
+	/**
+	 * Abstract class for the test functions
+	 * @author MOREPOWER
+	 *
+	 */
+	public static abstract class StatTestFunction {
+		public String name;
+
+		public StatTestFunction(String name) {
+			this.name = name;
+		}
+		
+		public abstract StatTracker runTest(BasicGraph g);
+		
+		public String toString() {
+			return name;
+		}
+	}
+	
+	/**
+	 * List of all the current analysis within this function
+	 */
+	public static StatTestFunction[] functions = {
+		new analyzeStrictDecreasingFunction(),
+		new analyzeDecreasingNeighbors(),
+		new analyzeStrictIncreasing(),
+		new analyzeIncreasingNeighbors(),
+		new analyzeNumColored(),
+		new analyzeHighColor()
+	};
+	
 	
 	/*
 	 * General notes:
@@ -25,40 +82,41 @@ public class Analysis {
 	 * analyzes vertices from high to low degree, then moving onto the 
 	 * next highest degree, ignoring coloring neighbors 
 	 */
-	
-	static void analyzeStrictDecreasing(BasicGraph g) {
-		long startTime = System.nanoTime();
-		int numColors = 0;
-		LinkedList<Node> unvisited = new LinkedList<Node>();
-		for (Node n : g.allNodes) {
-			unvisited.add(n);
+	public static class analyzeStrictDecreasingFunction extends StatTestFunction {
+
+		public analyzeStrictDecreasingFunction() {
+			super("analyzeStrictDecreasing");
 		}
 		
-		//colors will start from 1; 0 means not yet colored
-		while (!unvisited.isEmpty()) {
-			Node current = unvisited.pop();
-			color(current, g.allNodes.size());
-			if (current.color > numColors) {					
-				numColors++;
+		@Override
+		public StatTracker runTest(BasicGraph g) {
+			long startTime = System.nanoTime();
+			int numColors = 0;
+			LinkedList<Node> unvisited = new LinkedList<Node>();
+			for (Node n : g.allNodes) {
+				unvisited.add(n);
 			}
+			
+			//colors will start from 1; 0 means not yet colored
+			while (!unvisited.isEmpty()) {
+				Node current = unvisited.pop();
+				color(current, g.allNodes.size());
+				if (current.color > numColors) {					
+					numColors++;
+				}
+			}
+			
+			long endTime = System.nanoTime();
+			long duration = endTime - startTime;
+			
+			StatTracker tracker = new StatTracker(duration, numColors, verify(g));
+			if (tracker.my_correctness == false) {
+				throw new Error(this.name + " colored incorrectly!\n");
+			}
+			return tracker;
 		}
 		
-		long endTime = System.nanoTime();
-		long duration = endTime - startTime;
-		
-		System.out.println("Stricly Coloring from High to Low");
-		System.out.println("On a graph with " + g.allNodes.size() 
-				+ " vertices:");
-		System.out.println("Total Number of Colors Used: " + numColors);
-		System.out.println("Total Time For Analysis (in nanoseconds): " 
-				+ duration);
-		
-		System.out.println("Total Time For Analysis (in seconds): " 
-				+ duration/1000000000.0);
-		
-		System.out.println(verify(g));
-		System.out.println();
-	}
+	};
 	
 	/*
 	 * assuming g.allNodes is in decreasing sorted order by
@@ -66,125 +124,173 @@ public class Analysis {
 	 * colors its neighbors, then colors the vertex of next highest 
 	 * degree
 	 */
-	
-	static void analyzeDecreasingNeighbors(BasicGraph g) {
-		long startTime = System.nanoTime();
-		int numColors = 0;
-		LinkedList<Node> unvisited = new LinkedList<Node>();
-		for (Node n : g.allNodes) {
-			unvisited.add(n);
-		}
-		
-		//colors will start from 1; 0 means not yet colored
-		while (!unvisited.isEmpty()) {
-			Node current = unvisited.pop();
-			if (current.color == 0) {
-				color(current, g.allNodes.size());
-				if (current.color > numColors) {
-					numColors++;
-				}
-			}
-			
-			//This will color neighbors of current node
-			for (Node n : current.connections) {
-				if (n.color == 0) {
-					color(n, g.allNodes.size());
-					if (n.color > numColors) {
-						numColors++;
-					}
-				}
-			}
-		}
-		long endTime = System.nanoTime();
-		long duration = endTime - startTime;
-		
-		System.out.println("Coloring from High to Low, Coloring "
-				+ "Neighbors along the way");
-		System.out.println("On a graph with " + g.allNodes.size() 
-				+ " vertices:");
-		System.out.println("Total Number of Colors Used: " + numColors);
-		System.out.println("Total Time For Analysis (in nanoseconds): " 
-				+ duration);
-		System.out.println("Total Time For Analysis (in seconds): " 
-				+ duration/1000000000.0);
-		System.out.println(verify(g));
-		System.out.println();
-	}
-	
-	static void analyzeStrictIncreasing(BasicGraph g) {
-		long startTime = System.nanoTime();
-		int numColors = 0;
-		LinkedList<Node> unvisited = new LinkedList<Node>();
-		for (Node n : g.allNodes) {
-			unvisited.push(n);
-		}
-		
-		//colors will start from 1; 0 means not yet colored
-		while (!unvisited.isEmpty()) {
-			Node current = unvisited.pop();
-			color(current, g.allNodes.size());
-			if (current.color > numColors) {					
-				numColors++;
-			}
-		}
-		
-		long endTime = System.nanoTime();
-		long duration = endTime - startTime;
-		
-		System.out.println("Stricly Coloring from Low to High");
-		System.out.println("On a graph with " + g.allNodes.size() 
-				+ " vertices:");
-		System.out.println("Total Number of Colors Used: " + numColors);
-		System.out.println("Total Time For Analysis (in nanoseconds): " 
-				+ duration);
-		System.out.println("Total Time For Analysis (in seconds): " 
-				+ duration/1000000000.0);
-		System.out.println(verify(g));
-		System.out.println();
-	}
-	
-	static void analyzeIncreasingNeighbors(BasicGraph g) {
-		long startTime = System.nanoTime();
-		int numColors = 0;
-		LinkedList<Node> unvisited = new LinkedList<Node>();
-		for (Node n : g.allNodes) {
-			unvisited.push(n);
-		}
-		
-		//colors will start from 1; 0 means not yet colored
-		while (!unvisited.isEmpty()) {
-			Node current = unvisited.pop();
-			if (current.color == 0) {
-				color(current, g.allNodes.size());
-				if (current.color > numColors) {
-					numColors++;
-				}
-			}
-			
-			//This will color neighbors of current node
-			for (Node n : current.connections) {
-				if (n.color == 0) {
-					color(n, g.allNodes.size());
-					if (n.color > numColors) {
-						numColors++;
-					}
-				}
-			}
-		}
-		long endTime = System.nanoTime();
-		long duration = endTime - startTime;
+	public static class analyzeDecreasingNeighbors extends StatTestFunction {
 
-		System.out.println("Coloring from Low to High, Coloring "
-				+ "Neighbors along the way");
-		System.out.println("On a graph with " + g.allNodes.size() 
-				+ " vertices:");
-		System.out.println("Total Number of Colors Used: " + numColors);
-		System.out.println("Total Time For Analysis (in nanoseconds): " 
-				+ duration);
-		System.out.println("Total Time For Analysis (in seconds): " 
-				+ duration/1000000000.0);
-		System.out.println(verify(g));
-		System.out.println();
+		public analyzeDecreasingNeighbors() {
+			super("analyzeDecreasingNeighbors");
+		}
+		
+		@Override
+		public StatTracker runTest(BasicGraph g) {
+			long startTime = System.nanoTime();
+			int numColors = 0;
+			LinkedList<Node> unvisited = new LinkedList<Node>();
+			for (Node n : g.allNodes) {
+				unvisited.add(n);
+			}
+			
+			//colors will start from 1; 0 means not yet colored
+			while (!unvisited.isEmpty()) {
+				Node current = unvisited.pop();
+				if (current.color == 0) {
+					color(current, g.allNodes.size());
+					if (current.color > numColors) {
+						numColors++;
+					}
+				}
+				
+				//This will color neighbors of current node
+				for (Node n : current.connections) {
+					if (n.color == 0) {
+						color(n, g.allNodes.size());
+						if (n.color > numColors) {
+							numColors++;
+						}
+					}
+				}
+			}
+			long endTime = System.nanoTime();
+			long duration = endTime - startTime;
+			
+			StatTracker tracker = new StatTracker(duration, numColors, verify(g));
+			if (tracker.my_correctness == false) {
+				throw new Error(this.name + " colored incorrectly!\n");
+			}
+			return tracker;
+		}
+	};
+	
+	public static class analyzeStrictIncreasing extends StatTestFunction {
+
+		public analyzeStrictIncreasing() {
+			super("analyzeStrictIncreasing");
+		}
+		
+		@Override
+		public StatTracker runTest(BasicGraph g) {
+			long startTime = System.nanoTime();
+			int numColors = 0;
+			LinkedList<Node> unvisited = new LinkedList<Node>();
+			for (Node n : g.allNodes) {
+				unvisited.push(n);
+			}
+			
+			//colors will start from 1; 0 means not yet colored
+			while (!unvisited.isEmpty()) {
+				Node current = unvisited.pop();
+				color(current, g.allNodes.size());
+				if (current.color > numColors) {					
+					numColors++;
+				}
+			}
+			
+			long endTime = System.nanoTime();
+			long duration = endTime - startTime;
+			
+			StatTracker tracker = new StatTracker(duration, numColors, verify(g));
+			if (tracker.my_correctness == false) {
+				throw new Error(this.name + " colored incorrectly!\n");
+			}
+			return tracker;
+		}
+	};
+	
+	public static class analyzeIncreasingNeighbors extends StatTestFunction {
+
+		public analyzeIncreasingNeighbors() {
+			super("analyzeIncreasingNeighbors");
+		}
+		
+		@Override
+		public StatTracker runTest(BasicGraph g) {
+			long startTime = System.nanoTime();
+			int numColors = 0;
+			LinkedList<Node> unvisited = new LinkedList<Node>();
+			for (Node n : g.allNodes) {
+				unvisited.push(n);
+			}
+			
+			//colors will start from 1; 0 means not yet colored
+			while (!unvisited.isEmpty()) {
+				Node current = unvisited.pop();
+				if (current.color == 0) {
+					color(current, g.allNodes.size());
+					if (current.color > numColors) {
+						numColors++;
+					}
+				}
+				
+				//This will color neighbors of current node
+				for (Node n : current.connections) {
+					if (n.color == 0) {
+						color(n, g.allNodes.size());
+						if (n.color > numColors) {
+							numColors++;
+						}
+					}
+				}
+			}
+			long endTime = System.nanoTime();
+			long duration = endTime - startTime;
+
+			StatTracker tracker = new StatTracker(duration, numColors, verify(g));
+			if (tracker.my_correctness == false) {
+				throw new Error(this.name + " colored incorrectly!\n");
+			}
+			return tracker;
+		}
+	};
+
+	/*
+	 * Given a graph, it colors it based on the number of 
+	 * neighboring vertices that have previously been colored, from 
+	 * highest number colored to lowest.  It starts off by coloring 
+	 * the nodes of highest degree (number of vertices) and 
+	 * resorts the list after each vertex is colored.
+	 */
+	public static class analyzeNumColored extends StatTestFunction {
+		public analyzeNumColored() {
+			super("analyzeNumColored");
+		}
+		
+		@Override
+		public StatTracker runTest(BasicGraph g) {
+			long startTime = System.nanoTime();
+			int numColors = 0;
+			LinkedList<Node> unvisited = new LinkedList<Node>();
+			for (Node n : g.allNodes) {
+				unvisited.add(n);
+			}
+			
+			while (!unvisited.isEmpty()) {
+				Node current = unvisited.pop();
+				color(current, g.allNodes.size());
+				if (current.color > numColors) {					
+					numColors++;
+				}
+				//resorting nodes based on number of neighbors colored
+				Collections.sort(unvisited, BasicGraph.compareNumColored);
+			}
+			
+			long endTime = System.nanoTime();
+			long duration = endTime - startTime;
+
+			StatTracker tracker = new StatTracker(duration, numColors, verify(g));
+			if (tracker.my_correctness == false) {
+				throw new Error(this.name + " colored incorrectly!\n");
+			}
+			return tracker;
+		}
 	}
 	
 	/*
@@ -194,84 +300,45 @@ public class Analysis {
 	 * the nodes of highest degree (number of vertices) and 
 	 * resorts the list after each vertex is colored.
 	 */
-	static void analyzeNumColored(BasicGraph g) {
-		long startTime = System.nanoTime();
-		int numColors = 0;
-		LinkedList<Node> unvisited = new LinkedList<Node>();
-		for (Node n : g.allNodes) {
-			unvisited.add(n);
+	public static class analyzeHighColor extends StatTestFunction {
+		public analyzeHighColor() {
+			super("analyzeHighColor");
 		}
 		
-		while (!unvisited.isEmpty()) {
-			Node current = unvisited.pop();
-			color(current, g.allNodes.size());
-			if (current.color > numColors) {					
-				numColors++;
+		@Override
+		public StatTracker runTest(BasicGraph g) {
+			long startTime = System.nanoTime();
+			int numColors = 0;
+			LinkedList<Node> unvisited = new LinkedList<Node>();
+			for (Node n : g.allNodes) {
+				unvisited.add(n);
 			}
-			//resorting nodes based on number of neighbors colored
-			Collections.sort(unvisited, BasicGraph.compareNumColored);
-		}
-		
-		long endTime = System.nanoTime();
-		long duration = endTime - startTime;
-		
-		System.out.println("Coloring from High to Low, reordering nodes based on the number of neighbors colored:");
-		System.out.println("On a graph with " + g.allNodes.size() 
-				+ " vertices:");
-		System.out.println("Total Number of Colors Used: " + numColors);
-		System.out.println("Total Time For Analysis (in nanoseconds): " 
-				+ duration);
-		System.out.println("Total Time For Analysis (in seconds): " 
-				+ duration/1000000000.0);
-		System.out.println(verify(g));
-		System.out.println();
-	}
-	
-	/*
-	 * Given a graph, it colors it based on the number of 
-	 * neighboring vertices that have previously been colored, from 
-	 * highest number colored to lowest.  It starts off by coloring 
-	 * the nodes of highest degree (number of vertices) and 
-	 * resorts the list after each vertex is colored.
-	 */
-	static void analyzeHighColor(BasicGraph g) {
-		long startTime = System.nanoTime();
-		int numColors = 0;
-		LinkedList<Node> unvisited = new LinkedList<Node>();
-		for (Node n : g.allNodes) {
-			unvisited.add(n);
-		}
-		
-		while (!unvisited.isEmpty()) {
-			Node current = unvisited.pop();
-			color(current, g.allNodes.size());
-			if (current.color > numColors) {					
-				numColors++;
+			
+			while (!unvisited.isEmpty()) {
+				Node current = unvisited.pop();
+				color(current, g.allNodes.size());
+				if (current.color > numColors) {					
+					numColors++;
+				}
+				//resorting nodes based on highest color among neighbors
+				Collections.sort(unvisited, BasicGraph.compareHighColor);
 			}
-			//resorting nodes based on highest color among neighbors
-			Collections.sort(unvisited, BasicGraph.compareHighColor);
+			
+			long endTime = System.nanoTime();
+			long duration = endTime - startTime;
+			
+			StatTracker tracker = new StatTracker(duration, numColors, verify(g));
+			if (tracker.my_correctness == false) {
+				throw new Error(this.name + " colored incorrectly!\n");
+			}
+			return tracker;
 		}
-		
-		long endTime = System.nanoTime();
-		long duration = endTime - startTime;
-		
-		System.out.println("Coloring from High to Low, reordering nodes based on the highest color in neighbor set:");
-		System.out.println("On a graph with " + g.allNodes.size() 
-				+ " vertices:");
-		System.out.println("Total Number of Colors Used: " + numColors);
-		System.out.println("Total Time For Analysis (in nanoseconds): " 
-				+ duration);
-		System.out.println("Total Time For Analysis (in seconds): " 
-				+ duration/1000000000.0);
-		System.out.println(verify(g));
-		System.out.println();
 	}
 	
 	/* 
 	 * given an uncolored Node, colors it with the smallest color
 	 * not found in its neighbors
 	 */
-	
 	static void color(Node current, int size) {
 		//seeking the smallest unfilled color among already colored neighbors
 		boolean[] lowest = new boolean[size];
--- src/src/Runner.java
+++ src/src/Runner.java
@@ -3,46 +3,33 @@ package src;
 import java.io.File;
 import java.io.IOException;
 
+import src.Analysis.StatTracker;
+
 public class Runner {
 	
 	public static void main(String[] args) throws IOException {
+		
+		/**
+		 * TODO: Create a function that modulates the different components and fills
+		 * another mechanism that holds the tracker!
+		 * This can all be abstracted and replicated :D
+		 */
+		
 		
 		double density = 0.5;
 		String Output_Path = "../../../Desktop/EXPORTED_GRAPH/";
         BasicGraph graph = GraphCreator.createRandomConnectedGraph(25, 3, density);
         System.out.println(graph);
-        // Original Graph Export
-        // graph.export_graph(new File(Output_Path + "OG"));
-        // Strict Decreasing Export
-        Analysis.analyzeStrictDecreasing(graph);
-        if(!graph.export_graph(new File(Output_Path + "SD")))
-        	throw new Error("Export Failed");
-        graph.reset();
         
-        Analysis.analyzeDecreasingNeighbors(graph);
-        if(!graph.export_graph(new File(Output_Path + "DN")))
-        	throw new Error("Export Failed");
-        graph.reset();
-        
-        Analysis.analyzeIncreasingNeighbors(graph);
-        if(!graph.export_graph(new File(Output_Path + "IN")))
-        	throw new Error("Export Failed");
-        graph.reset();
-
-        Analysis.analyzeStrictIncreasing(graph);
-        if(!graph.export_graph(new File(Output_Path + "SI")))
-        	throw new Error("Export Failed");
-        graph.reset();
-        
-        Analysis.analyzeNumColored(graph);
-        if(!graph.export_graph(new File(Output_Path + "NC")))
-        	throw new Error("Export Failed");
-        graph.reset();
-        
-        Analysis.analyzeHighColor(graph);
-        if(!graph.export_graph(new File(Output_Path + "HC")))
-        	throw new Error("Export Failed");
-        
+        for (Analysis.StatTestFunction func : Analysis.functions) {
+        	System.out.println("Running Test " + func.toString());
+        	StatTracker stats = func.runTest(graph);
+        	System.out.println(stats.toString());
+        	if(!graph.export_graph(new File(Output_Path + func.name)))
+            	throw new Error("Export Failed");
+        	graph.reset();
+        }
+                
         // It worked!
         System.exit(0);
 	}
